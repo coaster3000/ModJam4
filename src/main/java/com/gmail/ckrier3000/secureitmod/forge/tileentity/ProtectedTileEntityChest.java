5ceee313bf5f83bd20e5ea1648122b07fc985054
Christopher Krier <ckrier.3000@gmail.com>
2014-05-16 19:01:35 -0400

    commit from 05/16/14 19:01:35. 0 deletions, 0 adds, 1 modifications, 0 new tracks

--- src/main/java/com/gmail/ckrier3000/secureitmod/forge/tileentity/ProtectedTileEntityChest.java
+++ src/main/java/com/gmail/ckrier3000/secureitmod/forge/tileentity/ProtectedTileEntityChest.java
@@ -6,9 +6,17 @@ import com.gmail.ckrier3000.secureitmod.forge.SecureItMod;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.Packet;
 import net.minecraft.tileentity.TileEntityChest;
+import net.minecraftforge.common.util.Constants.NBT;
 
 public class ProtectedTileEntityChest extends TileEntityChest {
+	
+	static final String COMPOUND_TAG_ID_CHEST_LOCK = "SILock";
+	static final String COMPOUND_TAG_ID_CHEST_LOCK_OWNER = "owner";
+	static final String COMPOUND_TAG_ID_CHEST_LOCK_ID = "lockId";
+	
+	
 	private String lockID;
 	private UUID owner;
 	
@@ -25,15 +33,34 @@ public class ProtectedTileEntityChest extends TileEntityChest {
 		adjacentChestZNeg = chest.adjacentChestZNeg;
 		adjacentChestZPos = chest.adjacentChestZPos;
 		tileEntityInvalid = chest.isInvalid();
+		xCoord = chest.xCoord;
+		yCoord = chest.yCoord;
+		zCoord = chest.zCoord;
+		for (int i = 0; i < chest.getSizeInventory(); i++)
+			setInventorySlotContents(i, chest.getStackInSlot(i));
 	}
 
 	@Override
 	public void readFromNBT(NBTTagCompound arg0) {
+		if (arg0.hasKey(COMPOUND_TAG_ID_CHEST_LOCK, NBT.TAG_COMPOUND)) {
+			lockID = arg0.getCompoundTag(COMPOUND_TAG_ID_CHEST_LOCK).getString(COMPOUND_TAG_ID_CHEST_LOCK_ID);
+			owner = UUID.fromString(arg0.getCompoundTag(COMPOUND_TAG_ID_CHEST_LOCK).getString(COMPOUND_TAG_ID_CHEST_LOCK_OWNER));
+		}
 		super.readFromNBT(arg0);
 	}
 	
 	@Override
+	public Packet getDescriptionPacket() {
+		return super.getDescriptionPacket();
+	}
+	
+	@Override
 	public void writeToNBT(NBTTagCompound arg0) {
+		NBTTagCompound tag = new NBTTagCompound();
+		tag.setString(COMPOUND_TAG_ID_CHEST_LOCK_ID, lockID);
+		tag.setString(COMPOUND_TAG_ID_CHEST_LOCK_OWNER, owner.toString());
+		
+		arg0.setTag(COMPOUND_TAG_ID_CHEST_LOCK, tag);
 		super.writeToNBT(arg0);
 	}
 }
