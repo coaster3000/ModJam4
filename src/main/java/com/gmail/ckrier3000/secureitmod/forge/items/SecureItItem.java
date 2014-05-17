e051f948e919be7b8f083cc8728d39f12f556368
Christopher Krier <ckrier.3000@gmail.com>
2014-05-17 14:49:57 -0400

    commit from 05/17/14 14:49:57. 0 deletions, 0 adds, 1 modifications, 0 new tracks

--- src/main/java/com/gmail/ckrier3000/secureitmod/forge/items/SecureItItem.java
+++ src/main/java/com/gmail/ckrier3000/secureitmod/forge/items/SecureItItem.java
@@ -1,14 +1,39 @@
 package com.gmail.ckrier3000.secureitmod.forge.items;
 
+import java.util.List;
+
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 
 public class SecureItItem extends Item {
-	public static SecureItItem lock, lockAndKey, key, unlocker;
-	
-	public static void init() {
-		
+
+	static enum Type {
+		keyAndLock, key, unlocker;
 	}
 	
 	public SecureItItem() {
+		setHasSubtypes(true);
+		setCreativeTab(CreativeTabs.tabTools);
+		setUnlocalizedName("SecureItItem");
+	}
+	
+	public Type getType(ItemStack stack) {
+		return Type.values()[stack.getItemDamage()%Type.values().length-1];
+	}
+	
+	@Override
+	public String getUnlocalizedName(ItemStack stack) {
+		return super.getUnlocalizedName() + getType(stack).name();
+	}
+	
+	@Override
+	public void getSubItems(Item item, CreativeTabs tabs, List ret) {
+		for (Type t: Type.values()) {
+			
+		}
+			
+
+			
 	}
 }
