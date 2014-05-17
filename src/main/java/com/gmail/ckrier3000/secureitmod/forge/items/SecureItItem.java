3cfb4e53f75664b85d4ea950f8988258d9e1cb46
Christopher Krier <ckrier.3000@gmail.com>
2014-05-17 14:34:55 -0400

    commit from 05/17/14 14:34:55. 0 deletions, 1 adds, 1 modifications, 1 new tracks

---
+++ src/main/java/com/gmail/ckrier3000/secureitmod/forge/items/SecureItItem.java
@@ -0,0 +1,14 @@
+package com.gmail.ckrier3000.secureitmod.forge.items;
+
+import net.minecraft.item.Item;
+
+public class SecureItItem extends Item {
+	public static SecureItItem lock, lockAndKey, key, unlocker;
+	
+	public static void init() {
+		
+	}
+	
+	public SecureItItem() {
+	}
+}
